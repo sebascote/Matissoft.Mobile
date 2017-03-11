d1d74c5b076154088169a79f753e7accfe540fdb
Sébastien Côté <sebastien.cote@matiss.com>
2017-03-11 08:28:43 -0500

    First commit

---
+++ RootViewController.designer.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Matissoft.Mobile
+{
+    [Register("RootViewController")]
+    partial class RootViewController
+    {
+        void ReleaseDesignerOutlets()
+        {
+        }
+    }
+}
+
